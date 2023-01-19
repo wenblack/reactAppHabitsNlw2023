883a9af27d10d59ee31d9e2f6d92b7f8d0ee852e
Wender Barbosa <wenblack012@gmail.com>
2023-01-18 22:23:06 -0300

    Create header componet

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,30 @@
 import { Habit } from './components/Habit'
-function App() {
+import './main.css'
+import logoImage from '../src/assets/logo.svg'
+import { Plus } from 'phosphor-react'
+export function App() {
 	return (
-		<>
-			<Habit completed={4} />
-			<Habit completed={3} />
-			<Habit completed={7} />
-			<Habit completed={2} />
-		</>
+		<div className='w-screen h-screen flex  justify-center items-center'>
+			<div className='w-full max-w-5xl px-6 flex flex-col gap-16'>
+				<header className='w-full max-w-3xl mx-auto flex items-center justify-between'>
+					<img
+						src={logoImage}
+						alt='logo Application'
+						height={16}
+						width={116}
+					/>
+					<button
+						type='button'
+						className='border border-violet-500 gap-3 font-semibold rounded-lg flex items-center px-6 py-4 hover:border-violet-300'
+					>
+						<Plus
+							className='text-violet-500'
+							size={20}
+						></Plus>
+						New habit
+					</button>
+				</header>
+			</div>
+		</div>
 	)
 }
-
-export default App
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ['./src/**/*.{html,js,tsx}'],
+	content: ['./src/**/*.{html,js,tsx}', './index.html'],
 	theme: {
-		extend: {}
+		extend: {
+			colors: {
+				background: '#09090A'
+			}
+		}
 	},
 	plugins: []
 }
