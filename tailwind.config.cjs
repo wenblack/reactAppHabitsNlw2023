cfdfb834459343b818ac84aeb189f02da945bbde
Wender Barbosa <wenblack012@gmail.com>
2023-01-22 22:02:16 -0300

    Lesson 05(Final version)

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,15 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ['./src/**/*.{html,js,tsx}', './index.html'],
-	theme: {
-		extend: {
-			colors: {
-				background: '#09090A'
-			},
-			gridTemplateRows: {
-				7: 'repeat(7, minmax(0, 1fr))'
-			}
-		}
-	},
-	plugins: []
+  content: [
+    './src/**/*.tsx',
+    './index.html'
+  ],
+  theme: {
+    extend: {
+      colors: {
+        background: '#09090A'
+      },
+
+      gridTemplateRows: {
+        7: 'repeat(7, minmax(0, 1fr))',
+      }
+    },
+  },
+  plugins: [],
 }
